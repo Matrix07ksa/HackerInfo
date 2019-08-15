ab6912a1b00d609e1cf32317d51941494aa0a424
Matrix <33636578+Matrix07ksa@users.noreply.github.com>
2019-08-15 20:22:02 +0300

    Update setup.py

--- HackerInfo_scan/setup.py
+++ HackerInfo_scan/setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup
-
+import pathlib
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
 setup(name='hackinfo',
-      version='1.0',
+      version='1.0.0',
       description='Hacker_Info',
-      url='https://github.com/Matrix07ksa/',
+      url='https://github.com/Matrix07ksa/HackerInfo/tree/master/HackerInfo_scan',
       author='Hejab Zaeri',
       author_email = "hjoooby9900@gmail.com", 
       license ='GBLV3',
